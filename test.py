4e50fbd4825c582581b5ed1f386b1f156897016d
emilhaldan <emilhaldan@live.dk>
2023-05-01 14:35:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,48 +1,31 @@
 import time
-from gpiozero import MCP3008
+import RPi.GPIO as GPIO
 
-adc = MCP3008(channel=0, device=0)  # Configure the MCP3008 ADC
+def rc_time(pin):
+    count = 0
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+    time.sleep(0.1)
 
-# Assuming GPIO14 is connected to CH2 and GPIO15 is connected to CH3 on the MCP3008
-gpio14_channel = 8
-gpio15_channel = 10
+    GPIO.setup(pin, GPIO.IN)
+    while GPIO.input(pin) == GPIO.LOW:
+        count += 1
+
+    return count
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Assuming the X output of the joystick is connected to GPIO14 and the Y output is connected to GPIO15
+x_axis_pin = 14
+y_axis_pin = 15
 
 try:
     while True:
-        gpio14_voltage = adc(channel = gpio14_channel).value * 3.3  # Convert the ADC reading to voltage (0-3.3V)
-        gpio15_voltage = adc(channel = gpio15_channel).value * 3.3  # Convert the ADC reading to voltage (0-3.3V)
-        
-        print("GPIO14 Voltage: {:.2f}V, GPIO15 Voltage: {:.2f}V".format(gpio14_voltage, gpio15_voltage))
+        x_axis_value = rc_time(x_axis_pin)
+        y_axis_value = rc_time(y_axis_pin)
+        print("X-axis: {}, Y-axis: {}".format(x_axis_value, y_axis_value))
         time.sleep(0.5)
 except KeyboardInterrupt:
+    GPIO.cleanup()
     print("Exiting")
-
-
-
-# import spidev
-# import time
-
-# def read_adc(adc, channel):
-#     if (channel > 7) or (channel < 0):
-#         return -1
-
-#     r = adc.xfer2([1, (8 + channel) << 4, 0])
-#     data = ((r[1] & 3) << 8) + r[2]
-#     return data
-
-# spi = spidev.SpiDev()
-# spi.open(0, 0)
-# spi.max_speed_hz = 1000000
-
-# x_axis_channel = 0
-# y_axis_channel = 1
-
-# try:
-#     while True:
-#         x_axis_value = read_adc(spi, x_axis_channel)
-#         y_axis_value = read_adc(spi, y_axis_channel)
-#         print("X-axis: {}, Y-axis: {}".format(x_axis_value, y_axis_value))
-#         time.sleep(0.2)
-# except KeyboardInterrupt:
-#     spi.close()
-
